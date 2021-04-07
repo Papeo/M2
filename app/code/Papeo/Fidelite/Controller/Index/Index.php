60a84f4c1dbf9e1d641af2e338aefd1853a62ff3
fredericazoulai <studio@papeo.fr>
2021-04-07 10:04:39 +0200

    27-12 // Création du module

---
+++ app/code/Papeo/Fidelite/Controller/Index/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Papeo\Fidelite\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('test index');
+    }
+}
+
---
+++ app/code/Papeo/Fidelite/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+// la seule chose à adapter c'est le Component Name
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Papeo_Fidelite', __DIR__);
+
+
